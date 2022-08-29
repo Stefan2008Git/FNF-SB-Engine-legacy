9de9628258341d5cfdbb3a0da3625e1c495d0e58
Sirox <71220271+Sirox228@users.noreply.github.com>
2022-08-29 13:18:57 +0300

    no more lag when pressing notes + working dialogue

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -5090,7 +5090,7 @@ class PlayState extends MusicBeatState
 			// had to do this because there is a bug in haxe where Stop != Continue doesnt work
 			var bool:Bool = ret == FunkinLua.Function_Continue;
 			if(!bool) {
-				returnVal = cast ret;
+				returnVal = ret;
 			}
 		}
 		#end
