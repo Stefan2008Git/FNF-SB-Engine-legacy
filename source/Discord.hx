51def32e3de63673c53fe64a42f42b9a3649df24
FearesterBETA <126818869+FearesterBETA@users.noreply.github.com>
2023-03-28 10:46:51 +0300

    Update Discord.hx

--- source/Discord.hx
+++ source/Discord.hx
@@ -6,6 +6,10 @@ import Sys.sleep;
 #if desktop
 import discord_rpc.DiscordRpc;
 #end
+#if LUA_ALLOWED
+import llua.Lua;
+import llua.State;
+#end
 using StringTools;
 
 class DiscordClient
@@ -92,4 +96,11 @@ class DiscordClient
 
 		//trace('Discord RPC Updated. Arguments: $details, $state, $smallImageKey, $hasStartTimestamp, $endTimestamp');
 	}
+        #if LUA_ALLOWED
+	public static function addLuaCallbacks(lua:State) {
+		Lua_helper.add_callback(lua, "changePresence", function(details:String, state:Null<String>, ?smallImageKey:String, ?hasStartTimestamp:Bool, ?endTimestamp:Float) {
+			changePresence(details, state, smallImageKey, hasStartTimestamp, endTimestamp);
+		});
+	}
+	#end
 }
