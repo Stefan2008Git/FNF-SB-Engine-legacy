bdf0bb0adbfee3427d02ec32c5d349348cc21483
Stefan2008Git <stefanbeta2008@gmail.com>
2023-04-04 22:38:27 +0200

    Currently a new SB Engine version...

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -25,7 +25,7 @@ using StringTools;
 
 class MainMenuState extends MusicBeatState
 {
-	public static var sbEngineVersion:String = '2.2.0'; //This is also used for Discord RPC
+	public static var sbEngineVersion:String = '2.3.0'; //This is also used for Discord RPC
 	public static var psychEngineVersion:String = '0.6.2';
 	public static var curSelected:Int = 0;
 
