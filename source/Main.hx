3363c292836ca9721fa0b16b1eb863009046f044
Stefan2008Git <stefanbeta2008@gmail.com>
2023-04-05 21:25:31 +0200

    Oops

--- source/Main.hx
+++ source/Main.hx
@@ -87,7 +87,7 @@ class Main extends Sprite
 			game.height = Math.ceil(stageHeight / game.zoom);
 		}
 	
-		SUtil.Check();
+		SUtil.check();
 	
 		ClientPrefs.loadDefaultKeys();
 		addChild(new FlxGame(game.width, game.height, game.initialState, #if (flixel < "5.0.0") game.zoom, #end game.framerate, game.framerate, game.skipSplash, game.startFullscreen));
