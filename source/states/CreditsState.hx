1b96f63b23cedc31e98cd248087c45a5bd4b2f32
Fearester2008 <131034025+Fearester2008@users.noreply.github.com>
2023-08-14 11:19:40 +0300

    fix FlxSpriteUtil

--- source/states/CreditsState.hx
+++ source/states/CreditsState.hx
@@ -335,7 +335,7 @@ class CreditsState extends MusicBeatState {
 		descBox.yAdd = -10;
 		descBox.alphaMult = 0.6;
 		descBox.alpha = 0.6;
-		FlxSpriteUtil.drawRoundRectComplex(descBox, 0, 0, 20, 20, 20, 20, 0x99000000);
+		FlxSpriteUtil.drawRoundRectComplex(descBox, 0, 0, 20, 20, 20, 20, 20, 20, 0x99000000);
 		add(descBox);
 
 		descText = new FlxText(50, FlxG.height + offsetThing - 25, 1180, "", 32);
