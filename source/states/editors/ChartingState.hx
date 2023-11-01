0535cb3e3f0401fc77782341cec55f66f1516330
Stefan Menićanin <126715034+Stefan2008Git@users.noreply.github.com>
2023-11-01 10:41:48 +0100

    Lel

--- source/states/editors/ChartingState.hx
+++ source/states/editors/ChartingState.hx
@@ -1677,7 +1677,7 @@ class ChartingState extends MusicBeatState {
 					    }
 					    // var funnySnap:Float = ((GRID_SIZE * getSectionBeats() * 4 * zoomList[curZoom]) + Conductor.stepCrochet / stepperStackOffset.value);
 					    for(i in 0...Std.int(addCount)) {
-						    addNote(currentlySelectedNote[0] + (_song.notes[curSec].changeBPM ? 15000/_song.notes[curSec].bpm : 15000/_song.bpm)/stepperStackOffset.value, currentlySelectedNote[1], currentTypE);
+						    addNote(currentlySelectedNote[0] + (_song.notes[curSec].changeBPM ? 15000 / _song.notes[curSec].bpm : 15000 / _song.bpm) / stepperStackOffset.value, currentlySelectedNote[1], currentType);
 					    }
 					}
 				}
@@ -1729,7 +1729,7 @@ class ChartingState extends MusicBeatState {
 					}
 					// var funnySnap:Float = ((GRID_SIZE * getSectionBeats() * 4 * zoomList[curZoom]) + Conductor.stepCrochet / stepperStackOffset.value);
 					for(i in 0...Std.int(addCount)) {
-						addNote(currentlySelectedNote[0] + (_song.notes[curSec].changeBPM ? 15000/_song.notes[curSec].bpm : 15000/_song.bpm)/stepperStackOffset.value, currentlySelectedNote[1], currentType);
+						addNote(currentlySelectedNote[0] + (_song.notes[curSec].changeBPM ? 15000 / _song.notes[curSec].bpm : 15000 / _song.bpm) / stepperStackOffset.value, currentlySelectedNote[1], currentType);
 					}
 				}
 			}
