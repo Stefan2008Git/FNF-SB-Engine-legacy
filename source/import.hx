ae5795613b0ca919feb39d14e5eb1de4c6b7cae8
Stefan2008_Official <126715034+Stefan2008Git@users.noreply.github.com>
2023-11-24 00:31:14 +0100

    Boi, the LanguageHandler is already imported
    Don't even do that again @Fearester2008

--- source/import.hx
+++ source/import.hx
@@ -63,7 +63,6 @@ import backend.Controls.Control;
 import backend.CoolUtil;
 import backend.CoolUtil.Countdown;
 import backend.CustomFadeTransition;
-import backend.LanguageHandler;
 #if desktop
 import backend.Discord.DiscordClient;
 #end
